3533079485accd0ad8d2ece73b0951d837c03fb4
Maclaurin Santos da Rocha <macrocha1239@gmail.com>
2023-10-15 01:42:03 -0300

    molhorando o mock de um db e criando um arquivo server para separa as minhas regras de negocios

--- js/index.js
+++ js/index.js
@@ -1,12 +1,22 @@
-const fetchTasksMock = async (resolve_or_reject) => {
-    const value = await new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if(resolve_or_reject) {
-                resolve(JSON.stringify([{ title: 'meu teste', status: 'pending' }]))
-            } else {
-                reject(JSON.stringify([{ message: 'Erro server!' }]))
-            }
-        }, 2000)
-    })
-    return value;
+import { fetchTasksMock, global_value_db } from './server'
+
+const deixa_a_requisicao = true;
+const tbody_element = document.querySelector('[data-tbody-tasks="tabela"]');
+
+
+
+const create_element = (tag, text_inside_element = '') => {
+    const element = document.createElement(tag);
+    const insert_in_element = document.createTextNode(text_inside_element)
+    element.appendChild(insert_in_element)
+    return element
 }
+
+const createRow = (task) => {
+    const { id, title, created_at, status } = tasks;
+
+    const tr = create_element('tr');
+    const tdtitle = create_element('td', 'titulo da task');
+}
+
+createRow({})
---
+++ js/server.js
@@ -0,0 +1,111 @@
+const global_value_db = [];
+const local_storage_escrever = (data) => {
+    const data_local_storage = JSON.stringify(data);
+    localStorage.setItem('db', data_local_storage);
+}
+
+const local_storage_pega = () => {
+    const get_data_local_storage = localStorage.getItem('db') || []
+    return get_data_local_storage;
+}
+
+const verb_get = async (resolve_or_reject) => {
+    const result = await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (resolve_or_reject) {
+                resolve(local_storage_pega())
+            } else {
+                reject(JSON.stringify([{ message: 'Erro server!' }]))
+            }
+        }, 2000)
+    })
+    return result
+}
+
+const verb_others = async (resolve_or_reject, data) => {
+    const result = await new Promise((resolve, reject) => {
+
+    })
+}
+
+const fetchTasksMock = async (verb_http = 'get', data) => {
+
+    switch (verb_http) {
+        case 'get':
+            const data_db = await verb_get(deixa_a_requisicao)
+            global_value_db = [...data_db];
+            break;
+        case 'put':
+            if (global_value_db.length == 0) {
+                const data_db = await verb_get(true);
+                global_value_db = [...data_db]
+                for (let i = 0; i < global_value_db.length; i++) {
+                    if (global_value_db[i].id == data.id) {
+                        global_value_db[i].title = data.title
+                        break;
+                    }
+                }
+                await verb_others(deixa_a_requisicao, global_value_db)
+            } else {
+                for (let i = 0; i < global_value_db.length; i++) {
+                    if (global_value_db[i].id == data.id) {
+                        global_value_db[i].title = data.title;
+                        break;
+                    }
+                }
+                await verb_others(deixa_a_requisicao, global_value_db);
+            }
+            break;
+        case 'post':
+            if (global_value_db.length == 0) {
+                const data_db = await verb_get(true)
+                global_value_db = [...data_db]
+                global_value_db.push(data)
+                await verb_others(deixa_a_requisicao, global_value_db)
+            } else {
+                global_value_db.push(data);
+                await verb_others(deixa_a_requisicao, global_value_db);
+            }
+            break;
+        case 'delete':
+            if (global_value_db.length == 0) {
+                const data_db = await verb_get(true);
+                const aux = data_db.filter(datas => datas.id !== data.id)
+                global_value_db = [...aux]
+                await verb_others(deixa_a_requisicao, aux)
+            } else {
+                const aux = global_value_db.filter(datas => datas.id !== data.id)
+                global_value_db = [...aux];
+                await verb_others(deixa_a_requisicao, aux);
+            }
+            break;
+        case 'patch':
+            if (global_value_db.length == 0) {
+                const data_db = await verb_get(true);
+                global_value_db = [...data_db]
+                for (let i = 0; i < global_value_db.length; i++) {
+                    if (global_value_db[i].id == data.id) {
+                        global_value_db[i].title = data.title
+                        break;
+                    }
+                }
+                await verb_others(deixa_a_requisicao, global_value_db)
+            } else {
+                for (let i = 0; i < global_value_db.length; i++) {
+                    if (global_value_db[i].id == data.id) {
+                        global_value_db[i].title = data.title;
+                        break;
+                    }
+                }
+                await verb_others(deixa_a_requisicao, global_value_db);
+            }
+            break;
+
+        default:
+            break;
+    }
+    return value;// retorno em string, precisa fazer JSON.parse(value)
+}
+
+
+exports = { fetchTasksMock, global_value_db }
